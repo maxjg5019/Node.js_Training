ce02467189a2225de130ee750f16e828c7f85b05
maxjg5019 <maxjg5019@gmail.com>
2022-12-01 17:39:55 +0800

    update

---
+++ index.js
@@ -0,0 +1,11 @@
+const http = require("http");
+
+const server = http.createServer(function (req, res) {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello Wordld");
+});
+
+server.listen("3000", function () {
+    console.log('server start on 3000 port')
+    
+});
